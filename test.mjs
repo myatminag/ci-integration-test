c374d875b5cb1e705eabac74424b89baf411fc89
MyatMinAung <myatminaung.dev@gmail.com>
2025-04-18 20:43:22 +0630

    ci: add workflow and integration test

---
+++ test.mjs
@@ -0,0 +1,14 @@
+import test from "node:test";
+import assert from "node:assert";
+
+test("test integer addition", async (t) => {
+  assert.strictEqual(1 + 1, 2, "1 + 1 should equal 2");
+});
+
+test("test string addition", async (t) => {
+  assert.strictEqual(
+    "1" + "1",
+    2,
+    "Concatenation of '1' and '1' does not equal 2"
+  );
+});
